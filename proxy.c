73bb6c77b5899d1edc229c47932aa32c91d70bb6
Baek Gi Chan <laphayen@gmail.com>
2023-09-21 01:44:07 +0900

    Update proxy.c

--- proxy.c
+++ proxy.c
@@ -6,16 +6,59 @@
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
 
+// LRU(Least Recently Used) 알고리즘 - 가장 오랫동안 참조되지 않은 페이지를 교체하는 기법
+#define LRU_MAGIC_NUMBER 9999
+
+#define CACHE_OBJS_COUNT 10
+
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
     "Firefox/10.0.3\r\n";
 
-void doit(int fd);
-int parse_uri(char *uri, char *request_ip, char *port, char *filename);
-void header_make(char *method, char *request_ip, char *user_agent_hdr, char *version, int clientfd, char *filename);
-void serve_static(int clientfd, int fd);
-void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
+static const char *requestline_hdr_format = "GET %s HTTP/1.0\r\n";
+static const char *endof_hdr = "\r\n";
+static const char *host_hdr_format = "Host: %s\r\n";
+static const char *conn_hdr = "Connection: close\r\n";
+static const char *prox_hdr = "Proxy-Connection: close\r\n";
+static const char *host_key = "Host";
+static const char *connection_key = "Connection";
+static const char *proxy_connection_key = "Proxy-Connection";
+static const char *user_agent_key = "User-Agent";
+
+void *thread(void *vargsp);
+void doit(int connfd);
+void parse_uri(char *uri, char *hostname, char *path, int *port);
+void build_http_header(char *http_header, char *hostname, char *path, int port, rio_t *client_rio);
+int connect_endServer(char *hostname, int port, char *http_header);
+
+// cache function
+void cache_init();
+int cache_find(char *url);
+void cache_uri(char *uri, char *buf);
+
+void readerPre(int i);
+void readerAfter(int i);
+
+typedef struct 
+{
+  char cache_obj[MAX_OBJECT_SIZE];
+  char cache_url[MAXLINE];
+  int LRU;
+  int isEmpty;
+
+  int readCnt;
+  sem_t wmutex;
+  sem_t rdcntmutex;
+}cache_block;
+
+
+typedef struct
+{
+  cache_block cacheobjs[CACHE_OBJS_COUNT];
+}Cache;
+
+Cache cache;
 
 int main(int argc, char **argv) {
   // 프록시 듣기 식별자, 프록시 연결 식별자
@@ -24,8 +67,11 @@ int main(int argc, char **argv) {
   char hostname[MAXLINE], port[MAXLINE];
   // 소켓 길이를 저장할 구조체
   socklen_t clientlen;
+  pthread_t tid;
   // 소켓 구조체 - clientaddress
   struct sockaddr_storage clientaddr;
+
+  cache_init(); 
 
   /* Check command line args */
   // 명령줄 인수를 확인하여 서버가 사용할 포트 번호를 결정
@@ -35,6 +81,8 @@ int main(int argc, char **argv) {
     fprintf(stderr, "usage: %s <port>\n", argv[0]);
     exit(1);
   }
+
+  Signal(SIGPIPE, SIG_IGN); 
 
   // 서버 소켓을 연다.
   // 지정된 포트 번호에서 클라이언트의 연결을 수신하기 위한 소켓 생성 후 반환
@@ -53,30 +101,36 @@ int main(int argc, char **argv) {
     Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
     printf("Accepted connection from (%s, %s)\n", hostname, port);
 
-    // 현재 연결에 대한 처리 
-    doit(connfd);   // line:netp:tiny:doit
-    // 연결 처리 후 소켓을 닫는다.
-    Close(connfd);  // line:netp:tiny:close
+    // 쓰레드 식별자, 쓰레드 특성, 쓰레드 함수, 쓰레드 함수 매개변수
+    Pthread_create(&tid, NULL, thread, (void *)connfd);
   }
   return 0;
 }
 
-void doit(int fd)
+void *thread(void *vargsp) {
+  int connfd = (int)vargsp;
+  Pthread_detach(pthread_self());
+  doit(connfd);
+  Close(connfd);
+}
+
+void doit(int connfd)
 {
-  int clientfd;
+  int end_serverfd;
   char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
-  char request_ip[MAXLINE], port[MAXLINE], filename[MAXLINE];
+  char endserver_http_header[MAXLINE];
+  char hostname[MAXLINE], path[MAXLINE];
+  int port;
   // 입출력 버퍼 초기화
   rio_t rio;
+  rio_t server_rio;
 
   // 클라이언트와의 통신을 위한 소켓 파일 디스크립터를 받는다. 
-  Rio_readinitb(&rio, fd); 
+  Rio_readinitb(&rio, connfd);
   // 요청 라인을 읽고 HTTP 요청 메소드, URI, 버전을 파싱한다.
   Rio_readlineb(&rio, buf, MAXLINE);
 
-  printf("Request headers: \n");
-  printf("%s", buf);
-
+  // 클라이언트의 요청 라인을 읽는다.
   sscanf(buf, "%s %s %s", method, uri, version);
 
   if (!(strcasecmp(method, "GET") == 0 || strcasecmp(method, "HEAD") == 0))
@@ -86,119 +140,244 @@ void doit(int fd)
     return;
   }
 
-  // 파싱 시작
-  parse_uri(uri, request_ip, port, filename);
-
-  // 헤더랑 응답 라인
-  clientfd = Open_clientfd(request_ip, port);
-  header_make(method, request_ip, user_agent_hdr, version, clientfd, filename);
-
-  serve_static(clientfd, fd);
-  Close(clientfd);
-}
-
-int parse_uri(char *uri, char *request_ip, char *port, char *filename)
-{
-  char *ptr;
-
-  // :의 아스키 코드
-  ptr = strchr(uri, 58);
-  // 포트가 있는 경우
-  if(ptr != NULL)
-  {
-    *ptr = '\0';
-    // 포트 앞의 아이피를 가져온다.
-    strcpy(request_ip, uri);
-    // 뒤의 포트와 나머지 경로를 가져온다.
-    strcpy(port, ptr+1);
-
-    // /의 아스키 코드 - 경로
-    ptr = strchr(port, 47);
-    // 경로가 있을 경우
-    if (ptr != NULL){
-      strcpy(filename, ptr);
-      *ptr = '\0';
-      strcpy(port, port);
-    }else{ // 경로가 없을 경우
-      strcpy(port, port);
-    }
+  if (strcasecmp(method, "GET")) {
+    printf("Proxy does not implement the method");
+    return;
   }
-  // 포트가 없을 경우
-  else
-  { 
-    strcpy(request_ip, uri);
-    ptr = strchr(request_ip, 47);
-    strcpy(filename, ptr+1);
-    port = "80";
+
+  char url_store[100];
+
+  strcpy(url_store, uri);
+
+  int cache_index;
+  if ((cache_index=cache_find(url_store)) != -1)
+  {
+    readerPre(cache_index);
+    Rio_writen(connfd, cache.cacheobjs[cache_index].cache_obj, strlen(cache.cacheobjs[cache_index].cache_obj));
+    readerAfter(cache_index);
+    return;
+  }
+  
+  parse_uri(uri, hostname, path, &port);
+
+  build_http_header(endserver_http_header, hostname, path, port, &rio);
+
+  end_serverfd = connect_endServer(hostname, port, endserver_http_header);
+  if (end_serverfd < 0)
+  {
+    printf("connection failed\n");
+    return;
+  }
+
+  Rio_readinitb(&server_rio, end_serverfd);
+
+  Rio_writen(end_serverfd, endserver_http_header, strlen(endserver_http_header));
+
+  char cachebuf[MAX_OBJECT_SIZE];
+  int sizebuf = 0;
+  size_t n;
+  while ((n=Rio_readlineb(&server_rio, buf, MAXLINE)) != 0)
+  {
+    sizebuf += n;
+    if (sizebuf < MAX_OBJECT_SIZE)
+      strcat(cachebuf, buf);
+    Rio_writen(connfd, buf, n);
+  }
+  Close(end_serverfd);
+
+  // store it
+  if (sizebuf < MAX_OBJECT_SIZE)
+  {
+    cache_uri(url_store, cachebuf);
   }
 }
 
-void header_make(char *method, char *request_ip, char *user_agent_hdr, char *version, int clientfd, char *filename)
+
+void build_http_header(char *http_header, char *hostname, char *path, int port, rio_t *client_rio) 
 {
-  char buf[MAXLINE];
+  char buf[MAXLINE], request_hdr[MAXLINE], other_hdr[MAXLINE], host_hdr[MAXLINE];
   
-  sprintf(buf, "%s %s %s\r\n", method, filename, "HTTP/1.0");
-  sprintf(buf, "%sHost: %s\r\n", buf, request_ip);
-  sprintf(buf, "%s%s", buf, user_agent_hdr);
-  sprintf(buf, "%sConnection: %s\r\n", buf, "close");
-  sprintf(buf, "%sProxy-Connection: %s\r\n\r\n", buf, "close");
+  sprintf(request_hdr, requestline_hdr_format, path);
 
-  Rio_writen(clientfd, buf, strlen(buf));
-}
-
-// 정적 파일을 클라이언트에게 제공하는 역할
-// 파일 이름과 크기에 따라 HTTP 응답 헤더를 생성하고
-// 파일 내용을 클라이언트에게 전송한다.
-void serve_static(int clientfd, int fd)
-{
-  int src_size;
-  char *srcp, *p, content_length[MAXLINE], buf[MAXBUF];
-  rio_t server_rio;
-  
-  // 버퍼 초기화 함수
-  Rio_readinitb(&server_rio, clientfd);
-  // 읽기
-  // 파일 fd에서 n바이트 크기를 읽어온다.
-  Rio_readlineb(&server_rio, buf, MAXLINE);
-  // 파일 내용을 클라이언트에게 전송 - 파일 내용을 클라이언트에게 쓰고, 파일 크기만큼 전송
-  Rio_writen(fd, buf, strlen(buf));
-
-  while(strcmp(buf, "\r\n"))
+  while (Rio_readlineb(client_rio, buf, MAXLINE) > 0) 
   {
-    if (strncmp(buf, "Content-length:", 15) == 0)
+    if (strcmp(buf, endof_hdr) == 0)
+      break;
+    
+    if (!strncasecmp(buf, host_key, strlen(host_key)))
     {
-      p = index(buf, 32);
-      strcpy(content_length, p+1);
-      src_size = atoi(content_length);
+      strcpy(host_hdr, buf);
+      continue;
     }
-  }
-  Rio_readlineb(&server_rio, buf, MAXLINE);
-  Rio_writen(fd, buf, strlen(buf));
 
-  // body
-  srcp = malloc(src_size);
-  Rio_readnb(&server_rio, srcp, src_size);
-  Rio_writen(fd, srcp, src_size);
-  free(srcp);
+    if (strncasecmp(buf, connection_key, strlen(connection_key))
+        &&strncasecmp(buf, proxy_connection_key, strlen(proxy_connection_key))
+        &&strncasecmp(buf, user_agent_key, strlen(user_agent_key)))
+        {
+        strcat(other_hdr, buf);
+      }
+  }
+  if (strlen(host_hdr) == 0) {
+    sprintf(host_hdr, host_hdr_format, hostname);
+  }
+  sprintf(http_header, "%s%s%s%s%s%s%s",
+          request_hdr,
+          host_hdr,
+          conn_hdr,
+          prox_hdr,
+          user_agent_hdr,
+          other_hdr,
+          endof_hdr);
+  return;
 }
 
-void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg)
+inline int connect_endServer(char *hostname, int port, char *http_header)
 {
-  char buf[MAXLINE], body[MAXBUF];
+  char portStr[100];
+  sprintf(portStr, "%d", port);
+  return Open_clientfd(hostname, portStr);
+}
 
-  sprintf(body, "<html><title>Tiny Error</title>");
-  sprintf(body, "%s<body bgcolor=""fffff"">\r\n", body);
-  sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
-  sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
-  sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
+void parse_uri(char *uri, char *hostname, char *path, int *port)
+{
+  *port = 80;
+  char *pos = strstr(uri, "//");
 
-  sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
-  Rio_writen(fd, buf, strlen(buf));
-  sprintf(buf, "Content-type: text/html\r\n");
-  Rio_writen(fd, buf, strlen(buf));
-  sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+  pos = pos!=NULL? pos+2:uri;
+
+  char *pos2 = strstr(pos, ":");
+
+  if (pos2 != NULL) {
+    *pos2 = '\0';
+    sscanf(pos, "%s", hostname);
+    sscanf(pos2+1, "%d%s", port, path);
+  } else {
+    pos2 = strstr(pos, "/");
+    if (pos2 != NULL) {
+      *pos2 = '\0';
+      sscanf(pos, "%s", hostname);
+      *pos2 = '/';
+      sscanf(pos2, "%s", path);
+    } else {
+      scanf(pos, "%s", hostname);
+    }
+  }
+  return;
+}
+
+void cache_init()
+{
+  cache.cache_num = 0;
+  int i;
+  for (i=0; i<CACHE_OBJS_COUNT; i++) 
+  {
+    cache.cacheobjs[i].LRU = 0;
+    cache.cacheobjs[i].isEmpty = 1;
+    Sem_init(&cache.cacheobjs[i].wmutex, 0, 1);
+    Sem_init(&cache.cacheobjs[i].rdcntmutex, 0, 1);
+    cache.cacheobjs[i].readCnt = 0;
+  }
+}
+
+void readerPre(int i) 
+{
+  P(&cache.cacheobjs[i].rdcntmutex);
+  cache.cacheobjs[i].readCnt++;
+  if (cache.cacheobjs[i].readCnt == 1)
+    P(&cache.cacheobjs[i].wmutex);
+  V(&cache.cacheobjs[i].rdcntmutex);
+}
+
+void readerAfter(int i) 
+{
+  P(&cache.cacheobjs[i].rdcntmutex);
+  cache.cacheobjs[i].readCnt--;
+  if (cache.cacheobjs[i].readCnt == 0)
+    V(&cache.cacheobjs[i].wmutex);
+  V(&cache.cacheobjs[i].rdcntmutex);
+}
+
+int cache_find(char *url) 
+{
+  int i;
+  for (i = 0; i < CACHE_OBJS_COUNT; i++) 
+  {
+    readerPre(i);
+    if (cache.cacheobjs[i].isEmpty == 0 && strcmp(url, cache.cacheobjs[i].cache_url) == 0)
+    {
+      readerAfter(i);
+      return i;
+    }
+    readerAfter(i);
+  }
+  return -1;
+}
+
+int cache_eviction() 
+{
+  int min = LRU_MAGIC_NUMBER;
+  int minindex = 0;
+  int i;
+  for (i=0; i<CACHE_OBJS_COUNT; i++) 
+  {
+    readerPre(i);
+    if (cache.cacheobjs[i].isEmpty == 1) 
+    {
+      minindex = i;
+      readerAfter(i);
+      break;
+    }
+    if (cache.cacheobjs[i].LRU < min) 
+    {
+      minindex = i;
+      min = cache.cacheobjs[i]. LRU;
+      readerAfter(i);
+      continue;
+    }
+    readerAfter(i);
+  }
+  return minindex;
+}
+
+void writePre(int i) 
+{
+  P(&cache.cacheobjs[i].wmutex);
+}
+
+void writeAfter(int i) 
+{
+  V(&cache.cacheobjs[i].wmutex);
+}
+
+void cache_LRU(int index) 
+{
+  int i;
+  for (i = 0; i < CACHE_OBJS_COUNT; i++) 
+  {
+    if (i == index) 
+    {
+      continue;
+    }
+    writePre(i);
+    if (cache.cacheobjs[i].isEmpty == 0) 
+    {
+      cache.cacheobjs[i].LRU--;
+    }
+    writeAfter(i);
+  }
+}
+
+void cache_uri(char *uri, char *buf) 
+{
+  int i = cache_eviction();
   
-  // HTTP 응답 헤더와 본문은 클라이언트에게 전송한다.
-  Rio_writen(fd, buf, strlen(buf));
-  Rio_writen(fd, body, strlen(body));
+  writePre(i);
+
+  strcpy(cache.cacheobjs[i].cache_obj, buf);
+  strcpy(cache.cacheobjs[i].cache_url, uri);
+  cache.cacheobjs[i].isEmpty = 0;
+  cache.cacheobjs[i].LRU = LRU_MAGIC_NUMBER;
+  cache_LRU(i);
+
+  writeAfter(i);
 }
